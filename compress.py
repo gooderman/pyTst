0471a72502467c969771d726df35819bfaacd796
gooderman <1121838283@qq.com>
2016-11-10 15:24:51 +0800

    png compress
    copy dir Resource to Resource_out
    compress Resource_out

---
+++ compress.py
@@ -0,0 +1,36 @@
+﻿import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+#set res dir
+resdir='Resources'
+resdst='Resources_out'
+os.system('cd ' + os.getcwd())
+os.system('rd '+ resdst + ' /S /Q')
+os.system('xcopy ' + resdir + '\\* ' + resdst + '\\ /S /R /E /Y')
+def isFileNeedEncytp(fileName):
+    if fileName.find('TileMap') > -1 :
+        print 'filter '+fileName
+        return False
+    if os.path.isdir(fileName):
+        return True		
+    if fileName.find('.png') > -1 :
+        return True
+    return False
+
+def folderEncytp(folderPath):
+	#print folderPath
+	if isFileNeedEncytp(folderPath):
+		for file in os.listdir(folderPath):
+			if os.path.isdir(folderPath + file + '\\'):
+				folderEncytp(folderPath + file + '\\')
+			else:
+				if isFileNeedEncytp(file):
+					fileName = folderPath + file
+					print 'compress '+ fileName
+					os.system(os.getcwd()+'\\pngquant -f --ext .png --quality 50-80 ' + '\"' + fileName + '\"')
+		return
+folderEncytp(os.getcwd() + '\\'+resdst+'\\')
+
+os.system("pause")
